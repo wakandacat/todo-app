5664f8c13e6006768f5736c1da872aac87a63642
Ellena Tzavelas <wakandacat@gmail.com>
2025-08-01 13:42:28 -0400

    added delete functionality, starting to style taskform

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -6,8 +6,9 @@ function Task(props){
 
     const {globalTasks, setGlobalTasks} = useContext(GlobalContext);
     const {refetchData, setRefetchData} = useContext(GlobalContext);
+    const {view, setView} = useContext(GlobalContext);
 
-    function completeTask(){
+    function updateTask(){
 
         //send the data in a json format to the backend
         fetch("http://localhost/todo-backend/update_task.php", {
@@ -17,6 +18,8 @@ function Task(props){
             },
             body: new URLSearchParams({
                 id: props.taskId,
+                task: props.text,
+                type: props.type,
                 completed: props.completed ? 0 : 1,
             })
         })
@@ -42,13 +45,43 @@ function Task(props){
         });
     }
 
+    //update button calls update_task.php with updated data
+
+    //delete button calls delete_task.php with the id of the task to delete
+    function deleteTask(){
+
+        //send the data in a json format to the backend
+        fetch("http://localhost/todo-backend/delete_task.php", {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/x-www-form-urlencoded" //form data so we can use POST
+            },
+            body: new URLSearchParams({
+                id: props.taskId, //only send the id of the task to delete
+            })
+        })
+        .then(response => response.text())
+        .then(data => {
+           console.log(data);
+
+            //let the TaskTable know that it needs to refetch the data
+            setRefetchData(prev => prev + 1);
+
+        });
+
+    }
+    
+    //tasks that are completed should be Archived at midnight and disappear from the table
+    //if the task is daily than create a new task with the same content
+    //if task is not completed than leave it be
+
     return(
         <tr className="task">
-            {props.completed ? (<td><button onClick={() => completeTask()} className="tableButton checked"><p>&#10003;</p></button></td>) : (<td><button onClick={() => completeTask()} className="tableButton"><p></p></button></td>)}
-            <td><h3>{props.text}</h3></td>
+            {props.completed ? (<td><button onClick={() => updateTask()} className="tableButton checked"><p>&#10003;</p></button></td>) : (<td><button onClick={() => updateTask()} className="tableButton"><p></p></button></td>)}
+            <td><h3>{props.taskId}&emsp;{props.text}</h3></td>
             <td><h3>{props.type}</h3></td>
-            <td><button className="tableButton">&#8593;</button></td>
-            <td><button className="tableButton">X</button></td>
+            <td><button className="tableButton" onClick={() => {setView("modifyTasks")}}>&#8593;</button></td> 
+            <td><button className="tableButton" onClick={() => deleteTask()}>X</button></td>
         </tr>
     );
 }
--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,11 +1,55 @@
 import React, {useState, useEffect, useContext} from "react";
 import '../styles/taskform.css';
+import GlobalContext from "./GlobalContext";
 
 function TaskForm(){
 
+    const {view, setView} = useContext(GlobalContext);
+
+    const [taskText, setTaskText] = useState("");
+    const [taskType, setTaskType] = useState("None");
+
+    function submitForm(){
+
+        // Prepare the data to be sent
+        const formData = new FormData();
+        formData.append('text', taskText.toUpperCase());
+        formData.append('type', taskType); // Default type, can be changed as needed
+
+        // Send the POST request to add a new task
+        fetch('http://localhost/todo-backend/add_task.php', {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+
+            console.log('Task added:', data);
+            setTaskText(""); // Clear the input field after submission
+        })
+        .catch(error => console.error('Error adding task:', error));
+
+        //switch back to view the table
+        setView("viewTasks");
+    }
+
+
     return (           
     <>
-        
+        <form onSubmit={(e) => {e.preventDefault(); submitForm();}}>
+            <label htmlFor="taskText"><h3>Set mission:</h3></label>
+            <input type="text" id="taskText" name="text" value={taskText}
+                onChange={(e) => setTaskText(e.target.value)}></input>
+            <label htmlFor="taskType"><h3>Set type:</h3></label>
+            <select id="taskType" name="type" value={taskType} onChange={(e) => setTaskType(e.target.value)}>
+                <option value="Daily">Daily</option>
+                <option value="Urgent">Urgent</option>
+                <option value="Stretch">Stretch</option>
+                <option value="None">None</option>
+            </select>
+            <br/>
+            <input type="submit" value="Submit" className="tableButton"></input>
+        </form>
     </>
     );
 }
