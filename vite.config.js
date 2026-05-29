25541b7b66aa65e285bb02bfbf27896558b96401
Ellena Tzavelas <wakandacat@gmail.com>
2026-05-29 15:34:12 -0400

    Set Vite base to /todo-app/ so GitHub Pages assets resolve

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Site is served from https://wakandacat.github.io/todo-app/ (a sub-path),
+  // so assets must be requested relative to /todo-app/, not the domain root.
+  base: '/todo-app/',
   plugins: [react()],
 })
